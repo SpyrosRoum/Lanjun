eebd8b008b6b9e4d3241dfbd3467b080a50c824d
Spyros Roum <spyros.roum@posteo.net>
2023-03-05 19:49:55 +0200

    [BE] Remove reservations and tables

--- lanjun-be/lanjun/entities/order.py
+++ lanjun-be/lanjun/entities/order.py
@@ -9,7 +9,6 @@ from lanjun.entities.links import OrderItemLink
 
 if TYPE_CHECKING:
     from lanjun.entities.item import Item
-    from lanjun.entities.reservation import Reservation
     from lanjun.entities.user import User
 
 
@@ -19,9 +18,7 @@ class Order(SQLModel, table=True):
     id: UUID = Field(default_factory=uuid4, primary_key=True, nullable=False)
     user_id: UUID = Field(nullable=False, foreign_key="users.id", index=True)
     cost: Decimal
-    prepaid: bool
     created_at: datetime
 
     user: "User" = Relationship(back_populates="orders")
-    reservation: "Reservation" = Relationship(back_populates="order")
     items: list["Item"] = Relationship(back_populates="orders", link_model=OrderItemLink)
